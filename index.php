3c8f3bf259004ddf8a397566cb5b7b6431cfbe10
Jorge Lopez <jorlopez@csumb.edu>
2018-08-27 22:35:40 +0000

    updated php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
-
-echo 'Jorge Lopez';
+ 
+echo'<h1> Jorge Lopez</h1>';
 
 ?>
 </body>
